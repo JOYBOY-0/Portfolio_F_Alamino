9a367e853d7b095f5a45870aa4e3614e3dcbed9c
Your Name <you@example.com>
2021-11-23 11:42:46 -0300

    add 404 and contact page

--- src/components/NotFound404.js
+++ src/components/NotFound404.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import anim from '../images/SCARECROW_1.gif'
 export default function NotFound404() {
     return (
-        <div>
-            
-        </div>
+        <section className="h-85vh md:h-80vh mt-15vh md:mt-20vh flex
+        flex flex-col items-center justify-center
+        md:justify-between md:flex-row w-scren">
+            <h1 className="text-3xl w-screen p-4 md:text-5xl font-bold
+            max-w-md text-gray-600">
+                Seems like this page does not exist.
+            </h1>
+            <img src={anim} alt="animation2" 
+            className="w-full max-w-lg"/>
+        </section>
     )
 }
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,8 +2,39 @@ import React from 'react'
 
 export default function Contact() {
     return (
-        <div>
-            
-        </div>
+        <section className="h-85vh md:h-80vh mt-15vh md:mt-20vh
+        flex flex-col w-scren overflow-hidden justify-between flex-wrap
+        p-8 md:p-20 lg:p-20vh">
+            <h1 className="text-4xl xs:text-5xl lg:text-6xl 
+            text-white font-bold pl-4
+            border-l-4">
+                I want to work <br />
+                <span className="text-blue-500"> with you.</span>
+            </h1>
+            <ul className="flex flex-col flex-wrap ml-auto
+            pr-4 border-r-4">
+                <a href="https://www.linkedin.com/in/francisco-alamino/" rel="noreferrer" target="_blank">
+                    <li className="flex items-center justify-end mb-4">
+                        <p className="mr-4 text-lg text-gray-700 font-semibold">Linkedin</p>
+                        <i className="fab fa-linkedin 
+                        text-5xl md:text-6xl text-gray-700"></i>
+                    </li>
+                </a>
+                <a href="https://www.linkedin.com/in/francisco-alamino/" rel="noreferrer" target="_blank">
+                    <li className="flex items-center justify-end mb-4">
+                        <p className="mr-4 text-lg text-gray-700 font-semibold">Call me</p>
+                        <i className="fas fa-phone-square 
+                        text-5xl md:text-6xl text-gray-700"></i>
+                    </li>
+                </a>
+                <a href="https://www.linkedin.com/in/francisco-alamino/" rel="noreferrer" target="_blank">
+                    <li className="flex items-center justify-end mb-4">
+                        <p className="mr-4 text-lg text-gray-700 font-semibold">Write me</p>
+                        <i className="fas fa-envelope 
+                        text-5xl md:text-6xl text-gray-700"></i>
+                    </li>
+                </a>
+            </ul>
+        </section>
     )
 }
